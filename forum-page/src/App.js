93222ba62ca2fadde222797308194de9c0456543
merwinfattah <18219019@std.stei.itb.ac.id>
2022-06-17 21:09:44 +0700

    work 17.06.22

--- forum-page/src/App.js
+++ forum-page/src/App.js
@@ -10,16 +10,18 @@ function App() {
         </header>
         <main className='container'>
           <section className='content'>
-            <article>
-              <h1>Lampu webcam tiba-tiba menyala sendiri tanpa membuka
-                aplikasi webcam
-              </h1>
-              <p>Mau tanya, akhir-akhir ini webcam sering nyala sendiri. 
-                Apakah ada yang tahu penyebabnya dan solusi mengatasi hal itu? Apakah ada
-                kemungkinan laptop saya di-hack karena kasus terjadi tiap terkoneksi di internet.</p>
-            </article>
+            <div className='topic'>
+              <article>
+                <h1>Lampu webcam tiba-tiba menyala sendiri tanpa membuka
+                  aplikasi webcam
+                </h1>
+                <p>Mau tanya, akhir-akhir ini webcam sering nyala sendiri. 
+                  Apakah ada yang tahu penyebabnya dan solusi mengatasi hal itu? Apakah ada
+                  kemungkinan laptop saya di-hack karena kasus terjadi tiap terkoneksi di internet.</p>
+              </article>
+            </div>
             <div className='comentainer'>
-              <div>
+              <div className='comentitle'>
                 Komentar
               </div>
               <div className='comentline'></div>
